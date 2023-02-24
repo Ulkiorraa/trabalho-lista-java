6d14fbe8687ad1a145ad699e55dab7e4d4c5f3ac
Fellipe J. P. Oliveira <fellipe_ulkiorra@live.com>
2023-02-24 13:20:09 -0300

    movendo função adicionarPessoas

--- src/listaDupla/listadupla.java
+++ src/listaDupla/listadupla.java
@@ -1,6 +1,7 @@
 package listaDupla;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class listadupla {
@@ -11,6 +12,36 @@ public class listadupla {
 
     public listadupla(){
         this.listaPessoa = new ArrayList<Pessoa>();
+    }
+
+    public void adicionarPessoa(Scanner ler, listadupla amigos) {
+        Pessoa pessoa = new Pessoa();
+        System.out.println("Digite o nome da Pessoa.");
+        pessoa.setNome(ler.nextLine());
+        System.out.println("Digite a idade da pessoa.");
+        pessoa.setIdade(ler.nextInt());
+        ler.nextLine();
+        System.out.println("Digite o telefone da pessoa.");
+        pessoa.setTelefone(ler.nextLine());
+        System.out.println("Deseja colocar os dados em cima ou em baixo da lista?");
+        System.out.println("1 - Adicionar ao começo da lista");
+        System.out.println("2 - Adicionar ao final da lista");
+        int opcao = ler.nextInt();
+        ler.nextLine();
+        
+        while (opcao != 1 && opcao != 2) {
+            System.out.println("Opção inválida! Digite novamente:");
+            opcao = ler.nextInt();
+            ler.nextLine();
+        }
+        
+        if (opcao == 1) {
+            amigos.addBegin(pessoa);
+            System.out.println("Dados adicionados no começo da lista!");
+        } else {
+            amigos.addEnd(pessoa);
+            System.out.println("Dados adicionados no fim da lista!");
+        }
     }
 
     public void addEnd(Pessoa ficha){
